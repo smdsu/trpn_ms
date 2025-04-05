9c48d8c290d5192bfde16cb5d8763e3e704472e9
smdsu <109036946+smdsu@users.noreply.github.com>
2025-04-05 16:20:35 +0300

    Edit main and confif

--- app/core/config.py
+++ app/core/config.py
@@ -1,2 +1,25 @@
-APP_NAME = "Tron MS"
-VERSION = "v0.0.1"
+from decouple import config
+import pydantic
+
+class Settings(pydantic.BaseSettings):
+    APP_NAME: str = "Tron MS"
+    VERSION: str = "v0.0.1"
+    TIMEZONE: str = "UTC"
+    DESCRIPTION: str | None = None
+    DEBUG: bool = False
+
+    DB_HOST: str = config("DB_HOST")
+    DB_PORT: int = config("DB_PORT")
+    DB_NAME: str = config("DB_NAME")
+    DB_USER: str = config("DB_USER")
+    DB_PASSWORD: str = config("DB_PASSWORD")
+
+
+settings = Settings()
+
+def get_db_url():
+    return (
+        f"postgresql+asyncpg://{settings.DB_USER}:"
+        f"{settings.DB_PASSWORD}@{settings.DB_HOST}:"
+        f"{settings.DB_PORT}/{settings.DB_NAME}"
+    )
--- app/main.py
+++ app/main.py
@@ -1,18 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.routing import APIRoute
 
-from app.core import config
+from app.core.config import settings
 
-
-
-def custom_generate_unique_id(route: APIRoute) -> str:
-    return f"{route.tags[0]}-{route.name}"
 
 app = FastAPI(
-    title=config.APP_NAME,
-    version=config.VERSION,
-    generate_unique_id_function=custom_generate_unique_id(),
+    title=settings.APP_NAME,
+    version=settings.VERSION,
 )
 
 app.add_middleware(

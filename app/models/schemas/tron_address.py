490b39e92513b04b1a2bb7d9dc890efe1554cfd9
smdsu <109036946+smdsu@users.noreply.github.com>
2025-04-06 14:26:35 +0300

    Fix pydantic schema

--- app/models/schemas/tron_address.py
+++ app/models/schemas/tron_address.py
@@ -3,12 +3,12 @@ from datetime import datetime
 from pydantic import BaseModel, ConfigDict, Field
 
 
-class TronAddress(BaseModel):
+class STronAddress(BaseModel):
     model_config = ConfigDict(from_attributes=True)    
-    id: int
-    address: str = Field(..., description="TRON wallet address")
+    id: int | None = Field(None, description="ID")
+    address: str | None = Field(None, description="TRON wallet address")
     bandwidth: int = Field(..., description="Bandwidth")
     energy: int = Field(..., description="Energy")
     balance: float = Field(..., description="Balance")
-    created_at: datetime = Field(..., description="Created at")
-    updated_at: datetime = Field(..., description="Updated at")
+    created_at: datetime | None = Field(None, description="Created at")
+    updated_at: datetime | None = Field(None, description="Updated at")

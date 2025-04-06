39e50678d8b4a33edf1bac81ba4f06f0b3df7d6e
smdsu <109036946+smdsu@users.noreply.github.com>
2025-04-06 15:44:48 +0300

    Fix uvicorn.run = app

--- app/main.py
+++ app/main.py
@@ -38,7 +38,7 @@ async def index():
 
 if __name__ == "__main__":
     uvicorn.run(
-        "main:app",
+        "app.main:app",
         host="0.0.0.0",
         port=8000,
         reload=settings.DEBUG,

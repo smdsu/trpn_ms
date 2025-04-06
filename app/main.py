247c54e8f2625a8f544a3829dc7ff03b7f738f0a
smdsu <109036946+smdsu@users.noreply.github.com>
2025-04-06 14:26:54 +0300

    Add pagination to the app

--- app/main.py
+++ app/main.py
@@ -1,13 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from fastapi_pagination import add_pagination
+
 from app.core.config import settings
+from app.api.endpoints import router as api_router
 
 
 app = FastAPI(
     title=settings.APP_NAME,
     version=settings.VERSION,
 )
+add_pagination(app)
 
 app.add_middleware(
     CORSMiddleware,
@@ -16,6 +20,8 @@ app.add_middleware(
     allow_headers=["*"],
     allow_credentials=True
 )
+
+app.include_router(api_router, prefix="/api")
 
 @app.get('/')
 async def index():
